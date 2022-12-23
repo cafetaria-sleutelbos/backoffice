10a13999ad7dcad0a6b19553db75288ea8d06e9c
Cahox <calvinhong123@gmail.com>
2022-12-23 12:53:15 +0100

    update

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->id();
             $table->text('two_factor_secret')
                     ->after('password')
                     ->nullable();
