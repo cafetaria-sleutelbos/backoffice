d104e119f458210b396958671ef2865472b75a51
Cahox <calvinhong123@gmail.com>
2023-01-05 04:01:44 +0100

    account seeder temp

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,15 +17,16 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ItemSeeder::class);
 
+        User::firstOrCreate([
+            'email' => 'admin@admin.admin'
+        ], [
+            'name' => 'admin',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
         if ((env('APP_ENV') ?? 'local') == 'local') {
-            User::firstOrCreate([
-                'email' => 'admin@admin.admin'
-            ], [
-                'name' => 'admin',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
-            ]);
 //            User::factory()->create([
 //                'name' => 'admin',
 //                'email' => 'admin@admin.admin'
